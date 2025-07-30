3f499b2cb5234d9adc17976e2b1a20d07f57e06f
poseiso <firdausfajar19@gmail.com>
2025-07-30 21:48:28 +0700

    feat(calc-state): add calculation to main loop

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -4,3 +4,4 @@ export const DEFAULT_PI_DIGIT: number = 2;
 
 export const PORT: number = Number(process.env.PORT) || 4000;
 export const COUNT_INTERVAL: number = Number(process.env.COUNT_INTERVAL) || 1000;
+export const MAX_DIGITS: number = Number(process.env.MAX_DIGITS) || Infinity;
