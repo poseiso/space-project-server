1bd56a5c27068ba9fa417c4c1bdd2f75067fa289
poseiso <firdausfajar19@gmail.com>
2025-07-30 21:41:17 +0700

    feat(pi-calc-function): register pi calculation to service

--- src/services/pi.services.ts
+++ src/services/pi.services.ts
@@ -1,5 +1,6 @@
 import { prisma } from '../db';
 import { SINGLE_PI_ID } from '../utils/const';
+import { computePi } from '../utils/pi_calculator';
 
 export async function getPi() {
   return prisma.pi.findUnique({ where: { id: SINGLE_PI_ID } });
@@ -10,6 +11,7 @@ export async function updatePi(value: string, digits: number) {
 }
 
 export function calculatePiToDigits(n: number): string {
-  // compute pi
-  return '3.1';
+  const result = computePi(n);
+  return result.value;
 }
+
